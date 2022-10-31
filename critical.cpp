bc8a1db3ca555f2f1893262871542517d151d3e8
Gustavo Silvera <grs5382@gmail.com>
2022-10-31 00:07:01 -0400

    updating to use a contentious counter

--- critical.cpp
+++ critical.cpp
@@ -9,69 +9,94 @@
 #define _LGPL_SOURCE 1
 #include <urcu/urcu-memb.h> // This is the preferred version of the library
 
-constexpr int num_readers = 10;
+constexpr size_t num_readers = 10;
 // constexpr int num_writers = 1;
 
-const int data_size = 1000 * 1000 * 100;
+float runtime = 1.f;         // second of total runtime
+int write_frequency_ms = 10; // /1000 to get seconds
+
+// this is the global!
+size_t gbl_counter = 0;
+bool running = true;
 std::mutex m;
-std::vector<float> data;
-bool running = true; // global flag to stop write thread on program completion
-std::vector<float> answers;
+
+void bump_counter()
+{
+    gbl_counter++;
+}
+
+void read_counter(size_t &var)
+{
+    var = gbl_counter;
+}
+
+struct ThreadData
+{
+    ThreadData(std::thread &&t, size_t _id) : thread(std::move(t)), id(_id)
+    {
+        read_counter(counter);
+    }
+    class std::thread thread;
+    size_t id = 0;
+    size_t counter = 0;
+    size_t ticks = 0;
+};
+std::vector<ThreadData> thread_data;
 
 void write_behavior()
 {
-    // repeatedly swaps two randomly selected elements
-    srand(0);
     while (running)
     {
-        int random_idx1 = rand() % data_size;
-        int random_idx2 = rand() % data_size;
-        std::swap(data[random_idx1], data[random_idx2]);
+        std::this_thread::sleep_for(std::chrono::milliseconds(write_frequency_ms));
+        bump_counter();
     }
 }
 
-void read_behavior(int id)
+void read_behavior(size_t id)
 {
-    const int amnt_read = data_size / num_readers;
-    int start = id * amnt_read;
-    float ret = 0.f;
-    for (int i = start; i < start + amnt_read; i++)
+    while (true)
     {
-        ret += data[i];
-    }
-    answers[id] = ret / 1000.f;
-}
-
-void init_data()
-{
-    data.resize(data_size);
-    srand(0); // seed at 0
-    for (int i = 0; i < data_size; i++)
-    {
-        // push random number from 0..100
-        data[i] = rand() % 100;
+        thread_data[id].ticks++;
+        if (thread_data[id].id != id)
+        {
+            std::cout << "FAIL: " << thread_data[id].id << "!=" << id << std::endl;
+        }
+        // read global counter
+        read_counter(thread_data[id].counter);
+        if (thread_data[id].counter >= runtime * (1000 / write_frequency_ms))
+        {
+            // std::lock_guard<std::mutex> lock(m);
+            // std::cout << "thread " << id << " finished!" << std::endl;
+            break;
+        }
     }
 }
 
 int main()
 {
-    init_data();
+    srand(0); // seed randomness
 
     std::thread writer = std::thread(write_behavior);
-    std::vector<std::thread> readers;
-    for (int i = 0; i < num_readers; i++)
+    thread_data.reserve(num_readers);
+    for (size_t i = 0; i < num_readers; i++)
     {
-        readers.push_back(std::thread(read_behavior, i));
-        answers.push_back(0.f);
+        thread_data[i] = ThreadData(std::thread(read_behavior, i), i);
     }
 
-    for (int id = 0; id < num_readers; id++)
+    // let it run for a while ...
+
+    for (size_t id = 0; id < num_readers; id++)
     {
-        readers[id].join();
-        std::cout << "section " << id << ": " << answers[id] << std::endl;
+        thread_data[id].thread.join();
     }
     running = false;
     writer.join();
+
+    // print final values
+    for (size_t id = 0; id < num_readers; id++)
+    {
+        std::cout << "thread " << id << ": " << thread_data[id].ticks << std::endl;
+    }
 
     return 0;
 }
