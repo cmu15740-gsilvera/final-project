463d5f23228322835db6aac1291949a49d0461b0
Gustavo Silvera <grs5382@gmail.com>
2022-10-31 18:40:16 -0400

    fixed rcu implemetation and timing fns

--- counter_rcu.cpp
+++ counter_rcu.cpp
@@ -1,7 +1,8 @@
-#include <pthread.h> // pthread_create
+#include <pthread.h> // pthread, mutex
 #include <stdbool.h> // bool
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> // timeval
 
 // userspace-RCU include
 #define _LGPL_SOURCE 1
@@ -18,31 +19,35 @@ volatile bool running = true;
 pthread_mutex_t write_mut;
 rcu_flavor_struct rcu;
 
+uint64_t get_current_time_us()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return 1000000ull * tv.tv_sec + tv.tv_usec;
+}
+
 void bump_counter()
 {
     // similar to https://www.kernel.org/doc/html/latest/RCU/whatisRCU.html#what-are-some-example-uses-of-core-rcu-api
-    // volatile size_t *new_counter;
-    // volatile size_t *old_counter;
-    // new_counter = (volatile size_t *)malloc(sizeof(volatile size_t));
-    // pthread_mutex_lock(&write_mut);
-    // old_counter = rcu_dereference(gbl_counter);
-    // *new_counter = (*old_counter + 1); // bump
-    // pthread_mutex_unlock(&write_mut);
-    // rcu_assign_pointer(gbl_counter, new_counter);
-    // urcu_memb_synchronize_rcu(); // synchronize_rcu();
-    // free((void *)old_counter);
-    (*gbl_counter)++;
+    volatile size_t *new_counter;
+    volatile size_t *old_counter;
+    new_counter = (volatile size_t *)malloc(sizeof(volatile size_t));
+    pthread_mutex_lock(&write_mut);
+    old_counter = rcu_dereference(gbl_counter);
+    *new_counter = (*old_counter + 1); // bump
+    rcu_assign_pointer(gbl_counter, new_counter);
+    pthread_mutex_unlock(&write_mut);
+    urcu_memb_synchronize_rcu(); // synchronize_rcu();
+    free((void *)old_counter);
+    // (*gbl_counter)++;
 }
 
 void read_counter(volatile size_t &var)
 {
-    // urcu_memb_read_lock();
-    // var = *rcu_dereference(gbl_counter);
-    // urcu_memb_read_unlock();
-    if (gbl_counter != NULL)
-    {
-        var = *gbl_counter;
-    }
+    urcu_memb_read_lock();
+    var = *rcu_dereference(gbl_counter);
+    urcu_memb_read_unlock();
+    // var = *gbl_counter;
 }
 
 struct ThreadData
@@ -51,45 +56,48 @@ struct ThreadData
     size_t id = 0;
     volatile size_t counter = 0;
     size_t ticks = 0;
-    float runtime = 0.f;
+    float runtime_s = 0.f;
 };
 
 ThreadData *thread_data = NULL;
 
 void *write_behavior(void *args)
 {
-    // urcu_memb_register_thread();
+    urcu_memb_register_thread();
     while (running)
     {
         bump_counter();
-        // volatile int x = 0;
-        // for (int i = 0; i < 1000; i++)
-        // {
-        //     x++;
-        // }
+
+        auto start = get_current_time_us();
+        float elapsed_ns = 0.f;
+        while (elapsed_ns < 10)
+        {
+            elapsed_ns = (get_current_time_us() - start) * 1000.f;
+        }
     }
-    // urcu_memb_unregister_thread();
+    urcu_memb_unregister_thread();
     return NULL;
 }
 
 void *read_behavior(void *args)
 {
     size_t id = *(size_t *)args;
-    // urcu_memb_register_thread();
+    urcu_memb_register_thread();
     if (id >= num_readers)
     {
         printf("Ouf of bounds! Id: %zu", id);
         return NULL;
     }
-    // printf("Starting thread %zu\n", id);
-    while (thread_data[id].ticks < 10 * 1000 * 1000)
+
+    auto start_t_us = get_current_time_us();
+    while (thread_data[id].ticks < 1 * 1000 * 1000)
     {
         thread_data[id].ticks++;
-        // read global counter
-        read_counter(thread_data[id].counter);
+        read_counter(thread_data[id].counter); // read global counter
     }
-    thread_data[id].runtime = 5.6;
-    // urcu_memb_unregister_thread();
+    auto end_t_us = get_current_time_us();
+    thread_data[id].runtime_s = (end_t_us - start_t_us) / 1e6;
+    urcu_memb_unregister_thread();
     return NULL;
 }
 
@@ -97,9 +105,11 @@ int main()
 {
     gbl_counter = (volatile size_t *)malloc(sizeof(size_t));
     (*gbl_counter) = 0;
-    srand(0); // seed randomness
-    // urcu_memb_init(); // rcu_init();
-    // pthread_mutex_init(&write_mut, NULL);
+    srand(0);
+    // seed randomness
+    urcu_memb_init(); // rcu_init();
+
+    pthread_mutex_init(&write_mut, NULL);
 
     pthread_t writer;
     if (pthread_create(&writer, NULL, write_behavior, NULL) != 0)
@@ -121,6 +131,7 @@ int main()
     }
 
     // let it run for a while ...
+    // sleep(1);
 
     for (size_t id = 0; id < num_readers; id++)
     {
@@ -133,14 +144,14 @@ int main()
     for (size_t id = 0; id < num_readers; id++)
     {
         auto &td = thread_data[id];
-        printf("thread %zu: %zu -- %.3fs -- %zu\n", id, td.ticks, td.runtime, td.counter);
+        printf("thread %zu: %zu -- %.3fs -- %zu\n", id, td.ticks, td.runtime_s, td.counter);
     }
     // final counter should be >= any of the threads' local counters in race mode
 
     printf("Final counter: %zu\n", *gbl_counter);
     free((void *)gbl_counter); // causes segfault??
     free((void *)thread_data);
-    // pthread_mutex_destroy(&write_mut);
+    pthread_mutex_destroy(&write_mut);
 
     return 0;
 }
